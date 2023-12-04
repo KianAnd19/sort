ec266fa159e55ea5e11f1b6476fa5aa92b2cd5fd
Kian Anderson <24872849@sun.ac.za>
2023-12-04 14:03:28 +0200

    Working pretty nicely, need to add highlught when its done

--- sorting-visualizer/main.js
+++ sorting-visualizer/main.js
@@ -3,22 +3,68 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('sortingCanvas');
+  const ctx = canvas.getContext('2d');
 
-setupCounter(document.querySelector('#counter'))
+  let array = [];
+  let sorting = false;
+
+  function initializeArray() {
+    const totalBars = 50;
+    const maxHeight = canvas.height;
+    array = Array.from({ length: totalBars }, (_, i) => (i / totalBars) * maxHeight);
+
+    // Optionally, shuffle the array here if you want the bars to start in a random order
+    array.sort(() => Math.random() - 0.5);
+
+    drawArray();
+}
+
+
+function drawArray() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  array.forEach((value, index) => {
+      ctx.fillStyle = 'blue';
+      ctx.fillRect(index * (canvas.width / array.length), canvas.height - value, (canvas.width / array.length) - 2, value);
+  });
+}
+
+function bubbleSort() {
+  let n = array.length;
+  let swapped;
+  let i = 0;
+  (function iterate(i) {
+      if (i < n) {
+          swapped = false;
+          (function innerIterate(j) {
+              if (j < n - i - 1) {
+                  if (array[j] > array[j + 1]) {
+                      [array[j], array[j + 1]] = [array[j + 1], array[j]];
+                      swapped = true;
+                  }
+                  drawArray();
+                  setTimeout(() => innerIterate(j + 1), 0);
+              } else if (swapped) {
+                  setTimeout(() => iterate(i + 1), 0);
+              }
+          })(0);
+      }
+  })(0);
+}
+
+
+  document.getElementById('startButton').addEventListener('click', () => {
+      if (!sorting) {
+          sorting = true;
+          bubbleSort();
+      }
+  });
+
+  document.getElementById('resetButton').addEventListener('click', () => {
+      sorting = false;
+      initializeArray();
+  });
+
+  initializeArray(); // Initialize array on page load
+});
