5727cc390cb17efaea779bf63ee763cadf41d495
Kian Anderson <24872849@sun.ac.za>
2023-12-07 14:31:31 +0200

    Changed bacgrounds of tiles to fix loading in error

--- sort-viz/src/components/HomeScreen.jsx
+++ sort-viz/src/components/HomeScreen.jsx
@@ -16,7 +16,7 @@ const HomeScreen = () => {
     <div className="p-5 bg-slate-200">
       <h1 className="text-3xl font-bold text-center mb-10">Sorting Algorithm Visualizer</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10">
-        <Link to="/selection-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s1.png)', backgroundSize: 'cover' }}>
+        <Link to="/selection-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s1.png)', backgroundSize: 'cover' }}>
           <div className="text-xl text-center font-medium text-black bg-slate-50 rounded-sm px-1 shadow-xl">Selection Sort</div>
         </Link>
         <Link to="/insertion-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s2.png)', backgroundSize: 'cover' }}>
@@ -37,16 +37,16 @@ const HomeScreen = () => {
         <Link to="/radix-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s7.png)', backgroundSize: 'cover' }}>
           <div className="text-xl font-medium text-black bg-slate-50 rounded-sm px-1 shadow-xl">Radix Sort</div>
         </Link>
-        <Link to="/bucket-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s8.png)', backgroundSize: 'cover' }}>
+        <Link to="/bucket-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s8.png)', backgroundSize: 'cover' }}>
           <div className="text-xl font-medium text-black bg-slate-50 rounded-sm px-1 shadow-xl">Bucket Sort</div>
         </Link>
-        <Link to="/shell-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s9.png)', backgroundSize: 'cover' }}>
+        <Link to="/shell-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s9.png)', backgroundSize: 'cover' }}>
           <div className="text-xl font-medium text-black bg-slate-50 rounded-sm px-1 shadow-xl">Shell Sort</div>
         </Link> 
-        <Link to="/counting-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s10.png)', backgroundSize: 'cover' }}>
+        <Link to="/counting-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s10.png)', backgroundSize: 'cover' }}>
           <div className="text-xl font-medium text-black bg-slate-50 rounded-sm px-1 shadow-xl">Counting Sort</div>
         </Link>
-        <Link to="/random-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s11.png)', backgroundSize: 'cover' }}>
+        <Link to="/random-sort" className="min-w-[200px] min-h-[200px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s11.png)', backgroundSize: 'cover' }}>
           <div className="text-xl font-medium text-black bg-slate-50 rounded-sm px-1 shadow-xl">Random Sort</div>
         </Link>
         {/* Add more sorting algorithm cards here */}
