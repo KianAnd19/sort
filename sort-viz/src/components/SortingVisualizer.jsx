7dd3c56d84fd1f0cc273726fae7ef5f322cc30c5
Kian Anderson <24872849@sun.ac.za>
2023-12-08 14:47:50 +0200

    The canvas now resizes to fit the width of the screen

--- sort-viz/src/components/SortingVisualizer.jsx
+++ sort-viz/src/components/SortingVisualizer.jsx
@@ -2,17 +2,30 @@ import React, { useState, useEffect, useRef } from 'react';
 
 const SortingVisualizer = ({ sortAlgorithm }) => {
     const [array, setArray] = useState([]);
+    const arrayRef = useRef(array);
     const canvasRef = useRef(null);
 
     useEffect(() => {
+        arrayRef.current = array;
+    }, [array]);
+
+    useEffect(() => {
         initializeArray();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
     }, []);
+
+    const handleResize = () => {
+        const canvas = canvasRef.current;
+        canvas.width = window.innerWidth;
+        drawArray(arrayRef.current); // Use arrayRef here
+    };
 
     const initializeArray = () => {
         const totalBars = 50;
         const maxHeight = 300;
         const newArray = Array.from({ length: totalBars }, (_, i) => Math.floor((i / totalBars) * maxHeight));
-        // Shuffle the array
+
         for (let i = newArray.length - 1; i > 0; i--) {
             const randomIndex = Math.floor(Math.random() * (i + 1));
             [newArray[i], newArray[randomIndex]] = [newArray[randomIndex], newArray[i]];
@@ -26,9 +39,11 @@ const SortingVisualizer = ({ sortAlgorithm }) => {
         const ctx = canvas.getContext('2d');
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+        const barWidth = canvas.width / arr.length;
+
         arr.forEach((value, index) => {
             ctx.fillStyle = index <= sortedIndex ? 'green' : 'blue';
-            ctx.fillRect(index * (canvas.width / arr.length), canvas.height - value, (canvas.width / arr.length) - 2, value);
+            ctx.fillRect(index * barWidth, canvas.height - value, barWidth - 2, value);
         });
     };
 
@@ -38,9 +53,9 @@ const SortingVisualizer = ({ sortAlgorithm }) => {
 
     return (
         <div>
-            <canvas ref={canvasRef} width={800} height={300}></canvas>
-            <button class="bg-green-500 rounded-md px-1 m-3" onClick={startSorting}>Start Sorting</button>
-            <button class="bg-red-500 rounded-md px-1 m-3" onClick={initializeArray}>Reset</button>
+            <canvas ref={canvasRef} width={window.innerWidth} height={300}></canvas>
+            <button className="bg-green-500 rounded-md px-1 m-3" onClick={startSorting}>Start Sorting</button>
+            <button className="bg-red-500 rounded-md px-1 m-3" onClick={initializeArray}>Reset</button>
         </div>
     );
 };
