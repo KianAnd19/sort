f8062f08cf4ebd5239fd97b65353da18fbb21abe
Kian Anderson <24872849@sun.ac.za>
2023-12-08 10:31:58 +0200

    Added header to the homescreen, also fixed merge sort tile

--- sort-viz/src/components/HomeScreen.jsx
+++ sort-viz/src/components/HomeScreen.jsx
@@ -1,12 +1,14 @@
 // src/components/HomeScreen.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 const HomeScreen = () => {
   return (
-    <div className="p-5 bg-slate-200">
-      <h1 className="text-3xl font-bold text-center mb-10">Sorting Algorithm Visualizer</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
+    <div className=" bg-slate-200">
+      <Header />
+      {/* <h1 className="text-3xl font-bold text-center mb-10">Sorting Algorithm Visualizer</h1> */}
+      <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
         <Link to="/selection-sort" className="min-w-[300px] min-h-[300px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s1.png)', backgroundSize: 'cover' }}>
           <div className="text-xl text-center font-medium text-black bg-slate-50 rounded-sm px-1 shadow-xl">Selection Sort</div>
         </Link>
@@ -19,7 +21,7 @@ const HomeScreen = () => {
         <Link to="/quick-sort" className="min-w-[300px] min-h-[300px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s4.png)', backgroundSize: 'cover' }}>
           <div className="text-xl font-medium text-black bg-slate-50 rounded-sm px-1 shadow-xl">Quick Sort</div>
         </Link>
-        <Link to="/merge-sort" className="min-w-[300px] min-h-[30px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s5.png)', backgroundSize: 'cover' }}>
+        <Link to="/merge-sort" className="min-w-[300px] min-h-[300px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s5.png)', backgroundSize: 'cover' }}>
           <div className="text-xl font-medium text-black bg-slate-50 rounded-sm px-1 shadow-xl">Merge Sort</div>
         </Link>
         <Link to="/heap-sort" className="min-w-[300px] min-h-[300px] p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex justify-center items-center space-x-4 hover:bg-gray-100" style={{ backgroundImage: 'url(/sort/s6.png)', backgroundSize: 'cover' }}>
